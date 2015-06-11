f3535b52a4f2af976854b14e7918a3cf022a0407
Jeffrey Linwood <jlinwood@gmail.com>
2015-06-10 20:23:11 -0500

    Fix formatting

--- www/js/phoneApp.js
+++ www/js/phoneApp.js
@@ -3,56 +3,55 @@ $(document).ready(function(){
   var tokenRequest    = new XMLHttpRequest();
   tokenRequest.onload = function() {
   
-  Twilio.Device.setup(tokenRequest.responseText);
-  var connection=null;
-  
-  
-  $("#dialButton").click(function() {
-                         params = { "tocall" : $('#phoneNumber').val()};
-                         connection = Twilio.Device.connect(params);
-                         });
-  
-  $("#hangupButton").click(function() {
-                           Twilio.Device.disconnectAll();
+    Twilio.Device.setup(tokenRequest.responseText);
+    var connection=null;
+    
+    
+    $("#dialButton").click(function() {
+                           params = { "tocall" : $('#phoneNumber').val()};
+                           connection = Twilio.Device.connect(params);
                            });
-  
-  
-  Twilio.Device.ready(function (device) {
-                      $('#statusMessage').text('Ready to start call');
-                      });
-  
-  Twilio.Device.incoming(function (conn) {
-                         if (confirm('Accept incoming call from ' + conn.parameters.From + '?')){
-                         connection=conn;
-                         conn.accept();
-                         }
-                         });
-  
-  Twilio.Device.offline(function (device) {
-                        //$('#statusMessage').text('Offline');
+    
+    $("#hangupButton").click(function() {
+                             Twilio.Device.disconnectAll();
+                             });
+    
+    
+    Twilio.Device.ready(function (device) {
+                        $('#statusMessage').text('Ready to start call');
                         });
-  
-  Twilio.Device.error(function (error) {
-                      $('#statusMessage').text(error);
-                      });
-  
-  Twilio.Device.connect(function (conn) {
-                        $('#statusMessage').text("Successfully established call");
-                        $('#dialButton').toggle();
-                        $('#hangupButton').toggle();
-                        
-                        });
-  
-  Twilio.Device.disconnect(function (conn) {
-                           $('#statusMessage').text("Call ended");
-                           $('#dialButton').toggle();
-                           $('#hangupButton').toggle();
+    
+    Twilio.Device.incoming(function (conn) {
+                           if (confirm('Accept incoming call from ' + conn.parameters.From + '?')){
+                            connection=conn;
+                            conn.accept();
+                            }
                            });
+    
+    Twilio.Device.offline(function (device) {
+                          //$('#statusMessage').text('Offline');
+                          });
+    
+    Twilio.Device.error(function (error) {
+                        $('#statusMessage').text(error);
+                        });
+    
+    Twilio.Device.connect(function (conn) {
+                          $('#statusMessage').text("Successfully established call");
+                          $('#dialButton').toggle();
+                          $('#hangupButton').toggle();
+                          });
+    
+    Twilio.Device.disconnect(function (conn) {
+                             $('#statusMessage').text("Call ended");
+                             $('#dialButton').toggle();
+                             $('#hangupButton').toggle();
+                             });
   };
-  
-  
+    
+    
   tokenRequest.open('GET', 'http://yourserver.example.com/twilio-token.php');
   tokenRequest.send();
   
-  });
+});
                                 
